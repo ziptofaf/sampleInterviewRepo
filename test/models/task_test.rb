8b1b749e8c8844d2ee636c7b1adff1a30c368f0b
ziptofaf <Marcin.Szmacinski@experian.com>
2023-08-28 10:02:31 +0200

    sample test

--- test/models/task_test.rb
+++ test/models/task_test.rb
@@ -1,7 +1,15 @@
 require "test_helper"
 
 class TaskTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "adding to the metrics" do
+    team = Team.create!(name: 'sample_team')
+    task = Task.create(description: 'sample task')
+    task.update!(team: team)
+    metric = DailyMetric.last
+    assert(metric.tasks_count == 1)
+
+    task = Task.create(description: 'sample task')
+    task.update!(team: team)
+    assert(metric.reload.tasks_count == 2)
+  end
 end
